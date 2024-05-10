9c591963776dc6cf5b24753f4504633a44a819d7
CharveeKrishna <72971435+CharveeKrishna@users.noreply.github.com>
2024-05-11 03:07:39 +0530

    Update pages.js repo url

--- pages.js
+++ pages.js
@@ -1,6 +1,6 @@
 const ghpages = require("gh-pages");
 const pathname = `${__dirname}/build`;
-const repoURL = "https://github.com/CharveeKrishna/my-portfolio.git";
+const repoURL = "https://github.com/CharveeKrishna/my-portfolio";
 
 ghpages.publish(
   pathname,
